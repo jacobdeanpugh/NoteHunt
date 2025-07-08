a0f30eb5a2e54e17b34f10da94319d0738ed7ed3
jacob pugh <155591283+jacobdeanpugh@users.noreply.github.com>
2025-07-07 22:10:08 -0700

    Added Basic Constructor To FileWatcherService

--- notehunt/src/main/java/dev/notequest/service/FileWatcherService.java
+++ notehunt/src/main/java/dev/notequest/service/FileWatcherService.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchService;
+import java.nio.file.WatchEvent;
 
 class FilesWatcherService {
     WatchService watchService;
     Path dirPath;
+
+    public static final WatchEvent.Kind<?>[] STANDARD_WATCH_EVENT_KINDS = {
+        StandardWatchEventKinds.ENTRY_CREATE,
+        StandardWatchEventKinds.ENTRY_DELETE,
+        StandardWatchEventKinds.ENTRY_MODIFY
+    };
 
     /*
     * FileWatcherService handles all functionality regarding analyzing and tracking file changes within a directory path.
@@ -21,10 +29,11 @@ class FilesWatcherService {
         try {
             this.watchService = FileSystems.getDefault().newWatchService();
             this.dirPath = Paths.get(directoryPath);
+
+            dirPath.register(watchService, STANDARD_WATCH_EVENT_KINDS);
         } catch (IOException e) {
             throw new RuntimeException("Failed to initialize WatchService", e);
         }
     }
 
-    
 }
