4266c309c65a9218befb71b40e7e5f535d6a252b
jacobdeanpugh <155591283+jacobdeanpugh@users.noreply.github.com>
2025-07-11 16:38:34 -0700

    - Updated H2 To Be Peristent

--- notehunt/src/main/java/dev/notequest/doa/DatabaseHandler.java
+++ notehunt/src/main/java/dev/notequest/doa/DatabaseHandler.java
@@ -2,9 +2,11 @@ package dev.notequest.doa;
 
 import java.sql.*;
 
+import java.io.File;
+
 
 public class DatabaseHandler {
-    private static final String CONNECTION_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String CONNECTION_URL = "jdbc:h2:file:./data/db;AUTO_SERVER=TRUE;DB_CLOSE_DELAY=-1";
     private static final String CONNECTION_USER = "sa";
     private static final String CONNECTION_PSWD = "";
 
@@ -12,9 +14,13 @@ public class DatabaseHandler {
 
     public DatabaseHandler() {
         try {
+            // Ensures the folder for database exists
+            new File("./data").mkdirs();
             this.conn = DriverManager.getConnection(CONNECTION_URL, CONNECTION_USER, CONNECTION_PSWD);
         } catch (SQLException e) {
             throw new RuntimeException("Unable to establish connection to databse: ", e);
+        } catch (SecurityException e) {
+            throw new RuntimeException("Unable to create folder due to permissions", e);
         }
     }
 
